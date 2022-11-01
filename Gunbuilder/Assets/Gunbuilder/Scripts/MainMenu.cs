5d29e9ddcecad773c880719f1a9a2378e020f556
Kirk Connor <kirkconnor2001@gmail.com>
2022-11-01 21:29:51 +0000

    Basic Main Menu
    Basic temp main menu, no Option functionality, can just Start and Quit game.

---
+++ Gunbuilder/Assets/Gunbuilder/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadScene(1); //Loads scene 1 in unity's build setting
+    }
+
+    public void ExitGame() 
+    {
+        Debug.Log("Quit Selected");
+        Application.Quit();
+    }
+}
